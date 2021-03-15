91b8fc5e55ac5a049a4ede73a5c2bc04e8e8f215
Minjun Kang <minjun@kaaang.dev>
2021-03-15 04:24:12 +0000

    시작 커밋

---
+++ Lab01/2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	unsigned short siX;
+	unsigned iX;
+	long liX;
+	long long lliX;
+	
+	cout << "sizeof(siX):" << sizeof(siX) << endl;
+}
---
+++ Lab02/1.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout <<  "My name is 강민준" << endl;
+}
